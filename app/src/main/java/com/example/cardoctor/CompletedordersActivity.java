c5f918d8749c1449548e5f1b57f3550c2387a670
Vihanga <vihangamalshan12346@gmail.com>
2020-08-17 01:05:53 +0530

    IT19111766

---
+++ app/src/main/java/com/example/cardoctor/CompletedordersActivity.java
@@ -0,0 +1,14 @@
+package com.example.cardoctor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CompletedordersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.completedorders);
+    }
+}
---
+++ app/src/main/java/com/example/cardoctor/ShippingordersActivity.java
@@ -0,0 +1,14 @@
+package com.example.cardoctor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShippingordersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.shippingorders);
+    }
+}
