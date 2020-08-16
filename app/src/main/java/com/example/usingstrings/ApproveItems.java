8d86ad650b99f90f79438cb9bd8b0f6306e91396
Savindu123 <an82334@gmail.com>
2020-08-16 23:36:09 +0530

    IT19019482

---
+++ app/src/main/java/com/example/usingstrings/ApproveItems.java
@@ -0,0 +1,14 @@
+package com.example.usingstrings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ApproveItems extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_approve_items);
+    }
+}
---
+++ app/src/main/java/com/example/usingstrings/PostedItems.java
@@ -0,0 +1,14 @@
+package com.example.usingstrings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PostedItems extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_posted_items);
+    }
+}
