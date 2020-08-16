47d2c0f3d51c5e22ef61a972af8f87f5d84c10aa
Maleesha Surajaa <malisha2019@gmail.com>
2020-08-16 13:58:30 -0400

    first edit

---
+++ CarDoctor/app/src/main/java/com/example/cardoctor/myAccountDetails.java
@@ -0,0 +1,14 @@
+package com.example.cardoctor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class myAccountDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_account_details);
+    }
+}
