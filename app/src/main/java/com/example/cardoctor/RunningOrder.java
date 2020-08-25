4b1116e11e227fc1aefa0268fe3c04eff2e9a9c5
Prasanja Udagedara <prasanja22231@gmail.com>
2020-08-25 20:14:51 +0530

    1st Commit

---
+++ app/src/main/java/com/example/cardoctor/RunningOrder.java
@@ -0,0 +1,14 @@
+package com.example.cardoctor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RunningOrder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_running_order);
+    }
+}
