4911abdb615dc06a7b9276cf77f9048f6fcbec01
Savindu123 <an82334@gmail.com>
2020-10-04 22:34:48 +0530

    commit 1

---
+++ app/src/androidTest/java/com/example/car_doctor/ExampleInstrumentedTest.java
@@ -0,0 +1,32 @@
+package com.example.car_doctor;
+
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    private ItemCalculationActivity itemCalculationActivity;
+
+    @Before
+    public void setup(){
+        itemCalculationActivity = new ItemCalculationActivity();
+    }
+    @Test
+    public void testCalcMonth(){
+        int result = itemCalculationActivity.clacMonthPayment(100,10,5);
+        assertEquals(50,result);
+    }
+}
